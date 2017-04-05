655aa0e9059b0e7a76eb4feffe794a2eefc5f146
Sam Roeca <sroeca@keplergrp.com>
2017-04-05 13:07:43 -0400

    Remove change directory command

--- tmuxlaunch.sh
+++ tmuxlaunch.sh
@@ -10,9 +10,8 @@ SESSION=SR
 # Create session in detached mode
 tmux new-session -d -s $SESSION
 
-# Change directory to source code and select first window
+# Select first window
 tmux select-window -t $SESSION:0
-tmux send-keys 'cd ~/src; clear' C-m
 
 # Rename first window to 'edit'
 tmux rename-window edit
