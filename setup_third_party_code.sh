e67a27cffa03ea0b9c9c1ee3aa92482e8bf90805
Sam Roeca <samuel.roeca@gmail.com>
2017-10-15 20:27:26 -0400

    Add pandoc install instructions, and notice

--- setup_third_party_code.sh
+++ setup_third_party_code.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
 # installs useful programs on system
+# assumes you are using Linux Mint
+# RECOMMENDATION:
+#   read document and type these commands yourself
+#   especially if you are using one of the latest Linux distributions.
+#   Things may have changed on the internet between now and your runtime.
 
 set -e
-
-INSTALL="apt install -y"
-REMOVE="apt remove -y"
 
 #######################################################################
 # Build, version control, and getting code for elsewhere
 #######################################################################
-$INSTALL git
-$INSTALL build-essential
-$INSTALL curl
+apt install git
+apt install build-essential
+apt install curl
 
 #######################################################################
 # Vim 8
 #######################################################################
 if [ ! -f /usr/bin/vim ]; then
-  $REMOVE vim
+  apt remove vim
 fi
 add-apt-repository ppa:jonathonf/vim
 apt update
-$INSTALL vim vim-nox
+apt install vim vim-nox
 # vim-plug: the best vim plugin manager as of December 25, 2016
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim curl
@@ -41,54 +43,54 @@ pip3 install --user --upgrade neovim
 #######################################################################
 # Vim dependencies
 #######################################################################
-$INSTALL exuberant-ctags
+apt install exuberant-ctags
 
 #######################################################################
 # Tmux
 #######################################################################
-$INSTALL tmux
+apt install tmux
 
 #######################################################################
 # System monitoring
 #######################################################################
-$INSTALL htop tree
+apt install htop tree
 
 #######################################################################
 # Python 3
 #######################################################################
-$INSTALL python3-dev
-$INSTALL python3-virtualenv
+apt install python3-dev
+apt install python3-virtualenv
 
 #######################################################################
 # MySQL
 #######################################################################
-$INSTALL libmysqlclient-dev
+apt install libmysqlclient-dev
 
 #######################################################################
 # Latex
 #######################################################################
-$INSTALL texlive-full
+apt install texlive-full
 
 #######################################################################
 # Nodejs
 #######################################################################
-$INSTALL python-software-properties
+apt install python-software-properties
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-$INSTALL nodejs
+apt install nodejs
 
 #######################################################################
 # Bluetooth
 #######################################################################
-$INSTALL blueman
+apt install blueman
 
 #######################################################################
 # Diagramming
 #######################################################################
-$INSTALL graphviz
-$INSTALL gthumb
+apt install graphviz
+apt install gthumb
 
 # graphy-easy
-$INSTALL libgraph-easy-perl
+apt install libgraph-easy-perl
 
 #######################################################################
 # PDF Viewer with vi bindings
@@ -96,13 +98,13 @@ $INSTALL libgraph-easy-perl
 #######################################################################
 add-apt-repository ppa:spvkgn/zathura-mupdf
 apt update
-$INSTALL mupdf
-$INSTALL zathura zathura-pdf-mupdf
+apt install mupdf
+apt install zathura zathura-pdf-mupdf
 
 #######################################################################
 # PDF Viewer with vi bindings
 #######################################################################
-$INSTALL python-pygments
+apt install python-pygments
 
 #######################################################################
 # PyEnv
@@ -110,3 +112,10 @@ $INSTALL python-pygments
 
 # dependencies for curses
 apt install libncurses5 libncurses5-dev libncursesw5
+
+#######################################################################
+# Pandoc
+#######################################################################
+# the available version in software repository is not latest
+# build using the provided debian package under pandoc releases
+# https://github.com/jgm/pandoc/releases
