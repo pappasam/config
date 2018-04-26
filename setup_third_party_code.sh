9441bd69fe0e3fed2f46d970789eece959f5b049
Sam Roeca <samuel.roeca@gmail.com>
2018-04-25 21:34:10 -0400

    Add install instructions for signal desktop

--- setup_third_party_code.sh
+++ setup_third_party_code.sh
@@ -210,3 +210,10 @@ go get github.com/cjbassi/gotop
 # pdfpc
 #######################################################################
 apt install pdf-presenter-console
+
+#######################################################################
+# Signal
+#######################################################################
+curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+sudo apt update && sudo apt install signal-desktop
