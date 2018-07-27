d8db9cdcf5767906ece650fa2f9b25f94f4ffc21
Sam Roeca <samuel.roeca@gmail.com>
2018-07-27 13:17:33 -0400

    Remove signal, add inkscape to thirt party code

--- setup_third_party_code.sh
+++ setup_third_party_code.sh
@@ -223,11 +223,9 @@ go get github.com/cjbassi/gotop
 apt install pdf-presenter-console
 
 #######################################################################
-# Signal
+# Inkscape (a great svg drawing program)
 #######################################################################
-curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
-echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-sudo apt update && sudo apt install signal-desktop
+snap install inkscape
 
 #######################################################################
 # zshell
