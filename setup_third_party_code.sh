ffc3320a3fe7c1beadda8a6dc1ce0ac391c33c9d
Sam Roeca <samuel.roeca@gmail.com>
2018-08-23 16:38:25 -0400

    Add instructions for vagrant

--- setup_third_party_code.sh
+++ setup_third_party_code.sh
@@ -121,6 +121,25 @@ sudo apt install xzdec
 sudo apt install fonts-firacode
 
 #######################################################################
+# Vagrant
+#######################################################################
+
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | \
+  sudo apt-key add -
+
+echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian bionic contrib" | \
+  sudo tee /etc/apt/sources.list.d/virtualbox.list
+
+sudo apt update
+sudo apt install virtualbox-5.2
+
+wget -O ~/Downloads/vagrant.deb \
+  https://releases.hashicorp.com/vagrant/2.1.2/vagrant_2.1.2_x86_64.deb
+
+sudo apt install ~/Downloads/vagrant.deb
+
+
+#######################################################################
 # Diagramming
 #######################################################################
 sudo apt install graphviz
