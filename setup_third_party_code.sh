a331eba5d901bc35b59ba9989b9bc1eda48df553
Sam Roeca <samuel.roeca@gmail.com>
2019-11-06 23:13:43 -0500

    Simplify installation scripts

--- setup_third_party_code.sh
+++ setup_third_party_code.sh
@@ -10,118 +10,33 @@
 set -e
 
 #######################################################################
-# Alternative package managers
-#######################################################################
-sudo apt install snapd
-
-#######################################################################
-# Man pages for Linux Systems Programming
-#######################################################################
-sudo apt install manpages-dev manpages-posix-dev
-sudo apt install libx11-doc
-
-# to obtain 'unbuffer', "disables the output buffering that occurs when program
-# output is redirected from non-interactive programs."
-# tl;dr this makes system
-# copy / url opening operations work when using neovim as a man pager
-sudo apt install expect-dev
-
-#######################################################################
-# Install packages
-#######################################################################
-sudo apt install stow
-
-sudo apt install shellcheck
-
-# Better annotations
-sudo apt install flameshot
-
-#######################################################################
-# Typing
-#######################################################################
-sudo apt install typespeed
-
-#######################################################################
-# Offline dictionary
-#######################################################################
-sudo apt install dict
-sudo apt install dict-gcide dict-moby-thesaurus
-
-#######################################################################
-# keepass
-#######################################################################
-# sudo apt install keepass2
-# sudo add-apt-repository ppa:dlech/keepass2-plugins
-# sudo apt update
-# sudo apt install keepass2-plugin-application-indicator
-# Ignore above, install this: https://keeweb.info/
-
-#######################################################################
 # Build, version control, and getting code for elsewhere
 #######################################################################
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt update
-sudo apt install git
-sudo apt install build-essential
-sudo apt install curl
-
-# Tool to respond to file changes
-sudo apt install entr
-
-#######################################################################
-# Copy functionality
-#######################################################################
-sudo apt install xsel xclip
+sudo apt install -y \
+  git \
+  build-essential \
+  curl \
+  entr \
+  stow
 
 #######################################################################
-# Fun stuff
+# Alternative package managers
 #######################################################################
-sudo apt install fortune cowsay bsdgames bsdgames-nonfree
+sudo apt install -y snapd
 
 #######################################################################
-# Install more C Stuff
+# zshell
 #######################################################################
-sudo apt install cmake llvm-6.0 llvm-6.0-dev libclang-6.0-dev
+sudo apt install -y zsh
+# without sudo (makes zsh default shell):
+chsh -s "$(which zsh)"
 
 #######################################################################
-# Apache stuff (htpasswd)
+# zplug
 #######################################################################
-sudo apt install apache2-utils
-
-#######################################################################
-# NeoVim
-#######################################################################
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt update
-sudo apt install neovim
-sudo apt install python-dev python-pip python3-dev python3-pip
-
-# Install minpac (neovim, for me, references vim)
-git clone https://github.com/kristijanhusak/vim-packager \
-  ~/.vim/pack/packager/opt/vim-packager
-
-# for ctags, after asdf
-# npm install -g jsctags
-# asdf: add php
-
-#######################################################################
-# Vim dependencies
-#######################################################################
-sudo apt install exuberant-ctags
-
-# Show key presses in the terminal
-sudo apt install screenkey
-
-#######################################################################
-# Fonts
-#######################################################################
-
-# Download relevant nerd fonts from here
-# https://www.nerdfonts.com/font-downloads
-
-# 1.) Download a Nerd Font
-# 2.) Unzip and copy to ~/.fonts
-# 3.) Run the command fc-cache -fv to manually rebuild the font cache
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 #######################################################################
 # Tmux - install from source to get latest version
@@ -136,15 +51,136 @@ sudo apt install screenkey
 # make
 # sudo make install
 
+# tpm: Tmux Plugin Manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+#######################################################################
+# ASDF
+#######################################################################
+
+# install dependencies
+sudo apt install \
+  automake autoconf libreadline-dev \
+  libncurses-dev libssl-dev libyaml-dev \
+  libxslt-dev libffi-dev libtool unixodbc-dev \
+  unzip curl
+
+# Download (verify version)
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.4
+
+# asdf-update: update to latest version
+# asdf reshim <plugin>: makes newly-installed executables available
+
+#######################################################################
+# Less
+#######################################################################
+# sudo apt remove less
+# Download latest recommended version from:
+# www.greenwoodsoftware.com/less/download.html
+# Follow installation instructions to compile from source
+
+#######################################################################
+# NeoVim
+#######################################################################
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install -y neovim
+
+# Install minpac (neovim, for me, references vim)
+git clone https://github.com/kristijanhusak/vim-packager \
+  ~/.vim/pack/packager/opt/vim-packager
+
+# for ctags, after asdf
+# npm install -g jsctags
+# asdf: add php
+
+#######################################################################
+# Man pages for Linux Systems Programming
+#######################################################################
+sudo apt install -y manpages-dev manpages-posix-dev
+sudo apt install -y libx11-doc
+
+# to obtain 'unbuffer', "disables the output buffering that occurs when program
+# output is redirected from non-interactive programs."
+# tl;dr this makes system
+# copy / url opening operations work when using neovim as a man pager
+sudo apt install -y expect-dev
+
+#######################################################################
+# Install packages
+#######################################################################
+sudo apt install -y shellcheck
+
+# Better annotations
+sudo apt install -y flameshot
+
+#######################################################################
+# Typing
+#######################################################################
+sudo apt install -y typespeed
+
+#######################################################################
+# Offline dictionary
+#######################################################################
+sudo apt install -y dict dict-gcide dict-moby-thesaurus
+
+#######################################################################
+# keepass
+#######################################################################
+# sudo apt install keepass2
+# sudo add-apt-repository ppa:dlech/keepass2-plugins
+# sudo apt update
+# sudo apt install keepass2-plugin-application-indicator
+# Ignore above, install this: https://keeweb.info/
+
+#######################################################################
+# Copy functionality
+#######################################################################
+sudo apt install -y xsel xclip
+
+#######################################################################
+# Fun stuff
+#######################################################################
+sudo apt install -y fortune cowsay bsdgames bsdgames-nonfree
+
+#######################################################################
+# Install more C Stuff
+#######################################################################
+sudo apt install -y cmake llvm-6.0 llvm-6.0-dev libclang-6.0-dev
+
+#######################################################################
+# Apache stuff (htpasswd)
+#######################################################################
+sudo apt install -y apache2-utils
+
+#######################################################################
+# Vim dependencies
+#######################################################################
+sudo apt install -y exuberant-ctags
+
+# Show key presses in the terminal
+sudo apt install -y screenkey
+
+#######################################################################
+# Fonts
+#######################################################################
+
+# Download relevant nerd fonts from here
+# https://www.nerdfonts.com/font-downloads
+
+# 1.) Download a Nerd Font
+# 2.) Unzip and copy to ~/.fonts
+# 3.) Run the command fc-cache -fv to manually rebuild the font cache
+
 #######################################################################
 # System monitoring
 #######################################################################
-sudo apt install htop tree
+sudo apt install -y htop tree
 
 #######################################################################
 # MySQL
 #######################################################################
-sudo apt install libmysqlclient-dev
+sudo apt install -y libmysqlclient-dev
 
 #######################################################################
 # Wine
@@ -161,10 +197,10 @@ sudo apt install libmysqlclient-dev
 #######################################################################
 # Latex
 #######################################################################
-sudo apt install texlive-full
+sudo apt install -y texlive-full
 
 # getting tlmgr (the texlive package manager) to work
-sudo apt install xzdec
+sudo apt install -y xzdec
 
 #######################################################################
 # Vagrant
@@ -188,8 +224,7 @@ sudo apt install xzdec
 #######################################################################
 # Diagramming
 #######################################################################
-sudo apt install graphviz
-sudo apt install gthumb
+sudo apt install -y graphviz gthumb
 
 # plantuml
 if [ ! -d ~/java ]; then
@@ -206,7 +241,7 @@ cd ~/bin
 #######################################################################
 # PDF Viewer with vi bindings
 #######################################################################
-sudo apt install zathura
+sudo apt install -y zathura
 
 #######################################################################
 # Pandoc
@@ -214,7 +249,7 @@ sudo apt install zathura
 # the available version in software repository is not latest
 # build using the provided debian package under pandoc releases
 # https://github.com/jgm/pandoc/releases
-sudo apt install librsvg2-bin
+sudo apt install -y librsvg2-bin
 
 #######################################################################
 # Writing
@@ -257,8 +292,8 @@ sudo apt install librsvg2-bin
 #######################################################################
 # Ncurses
 #######################################################################
-sudo apt install libncurses5 libncurses5-dev libncursesw5
-sudo apt install ncurses-doc
+sudo apt install -y libncurses5 libncurses5-dev libncursesw5
+sudo apt install -y ncurses-doc
 
 #######################################################################
 # Rust packages / rustup components
@@ -275,13 +310,12 @@ sudo apt install ncurses-doc
 
 # cargo libraries
 
-cargo install bat
-cargo install fd-find
-cargo install ripgrep
-cargo install cargo-deb
-
-# supports the following: cargo add; cargo rm ;cargo upgrade
-cargo install cargo-edit
+cargo install \
+  bat \
+  fd-find \
+  ripgrep \
+  cargo-deb \
+  cargo-edit
 
 #######################################################################
 # Go programs
@@ -311,7 +345,7 @@ cargo install cargo-edit
 #######################################################################
 sudo add-apt-repository ppa:peek-developers/stable
 sudo apt update
-sudo apt install peek
+sudo apt install -y peek
 
 #######################################################################
 # Alacritty
@@ -319,19 +353,9 @@ sudo apt install peek
 cd src/lib
 git clone git@github.com:eendroroy/alacritty-theme.git
 git clone git@github.com:toggle-corp/alacritty-colorscheme.git
-# ln -s $PWD/alacritty-colorscheme/alacritty-colorscheme $HOME/bin/
+ln -s $PWD/alacritty-colorscheme/alacritty-colorscheme $HOME/bin/
 
-sudo apt install libxcb-xfixes0-dev
-
-#######################################################################
-# zplug
-#######################################################################
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-
-#######################################################################
-# tpm: Tmux Plugin Manager
-#######################################################################
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sudo apt install -y libxcb-xfixes0-dev
 
 #######################################################################
 # AWS
@@ -359,13 +383,6 @@ pip install restview
 # sudo snap install inkscape
 
 #######################################################################
-# zshell
-#######################################################################
-sudo apt install zsh
-# without sudo (makes zsh default shell):
-chsh -s "$(which zsh)"
-
-#######################################################################
 # Jenkins
 #######################################################################
 wget -O ~/java/jenkins.war \
@@ -376,27 +393,3 @@ cd ~/bin
 # #!/bin/bash
 # java -jar ~/java/jenkins.war ${@}
 
-#######################################################################
-# Less
-#######################################################################
-# sudo apt remove less
-# Download latest recommended version from:
-# www.greenwoodsoftware.com/less/download.html
-# Follow installation instructions to compile from source
-
-#######################################################################
-# ASDF
-#######################################################################
-
-# install dependencies
-sudo apt install \
-  automake autoconf libreadline-dev \
-  libncurses-dev libssl-dev libyaml-dev \
-  libxslt-dev libffi-dev libtool unixodbc-dev \
-  unzip curl
-
-# Download (verify version)
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.4
-
-# asdf-update: update to latest version
-# asdf reshim <plugin>: makes newly-installed executables available
