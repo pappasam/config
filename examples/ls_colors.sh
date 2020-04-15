87fcd803febb1aa51905a57cc5b2cadb079073c1
Sam Roeca <samuel.roeca@gmail.com>
2020-04-15 10:58:58 -0400

    Remove LS_COLORS configuration.
    Not necessary anymore in zsh; removing simplifies zshrc

---
+++ examples/ls_colors.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Example configuration for LS_COLORS environment variable
+
+# Colors when using the LS command
+# NOTE:
+# Color codes:
+#   0   Default Colour
+#   1   Bold
+#   4   Underlined
+#   5   Flashing Text
+#   7   Reverse Field
+#   31  Red
+#   32  Green
+#   33  Orange
+#   34  Blue
+#   35  Purple
+#   36  Cyan
+#   37  Grey
+#   40  Black Background
+#   41  Red Background
+#   42  Green Background
+#   43  Orange Background
+#   44  Blue Background
+#   45  Purple Background
+#   46  Cyan Background
+#   47  Grey Background
+#   90  Dark Grey
+#   91  Light Red
+#   92  Light Green
+#   93  Yellow
+#   94  Light Blue
+#   95  Light Purple
+#   96  Turquoise
+#   100 Dark Grey Background
+#   101 Light Red Background
+#   102 Light Green Background
+#   103 Yellow Background
+#   104 Light Blue Background
+#   105 Light Purple Background
+#   106 Turquoise Background
+# Parameters
+#   di 	Directory
+LS_COLORS="di=1;34:"
+#   fi 	File
+LS_COLORS+="fi=0:"
+#   ln 	Symbolic Link
+LS_COLORS+="ln=1;36:"
+#   pi 	Fifo file
+LS_COLORS+="pi=5:"
+#   so 	Socket file
+LS_COLORS+="so=5:"
+#   bd 	Block (buffered) special file
+LS_COLORS+="bd=5:"
+#   cd 	Character (unbuffered) special file
+LS_COLORS+="cd=5:"
+#   or 	Symbolic Link pointing to a non-existent file (orphan)
+LS_COLORS+="or=31:"
+#   mi 	Non-existent file pointed to by a symbolic link (visible with ls -l)
+LS_COLORS+="mi=0:"
+#   ex 	File which is executable (ie. has 'x' set in permissions).
+LS_COLORS+="ex=1;92:"
+# additional file types as-defined by their extension
+LS_COLORS+="*.rpm=90"
+
+# Finally, export LS_COLORS
+export LS_COLORS
