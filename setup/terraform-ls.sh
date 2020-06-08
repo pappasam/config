404f8a15b5673ce54afc75ba72014b2a7cd3e49b
Sam Roeca <samuel.roeca@gmail.com>
2020-06-08 12:28:44 -0400

    Add terraform-ls to language server setup

---
+++ setup/terraform-ls.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VERSION="0.3.2"
+DOWNLOAD_FILE="$HOME/Downloads/terraform.zip"
+
+curl -o "$DOWNLOAD_FILE" \
+  "https://releases.hashicorp.com/terraform-ls/$VERSION/terraform-ls_${VERSION}_linux_amd64.zip"
+
+unzip -d "$HOME/bin" "$DOWNLOAD_FILE"
