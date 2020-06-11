ef564c74b131a823d5f7a48edbd82f123e4056f6
Sam Roeca <samuel.roeca@gmail.com>
2020-06-11 11:25:37 -0400

    Update language versions

--- setup/asdf-install.sh
+++ setup/asdf-install.sh
@@ -7,8 +7,8 @@
 
 asdf plugin-add nodejs
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
-asdf install nodejs 14.2.0
-asdf global nodejs 14.2.0
+asdf install nodejs 14.4.0
+asdf global nodejs 14.4.0
 
 ###########################################################################
 # Generalized
@@ -19,13 +19,13 @@ asdf_setup() {
   asdf global "$1" "$2"
 }
 
-asdf_setup golang 1.13.4
+asdf_setup golang 1.14.4
 asdf_setup java adopt-openjdk-13.0.2+8
 asdf_setup neovim nightly
-asdf_setup php 7.4.5
-asdf_setup python 3.8.2
+asdf_setup php 7.4.7
+asdf_setup python 3.8.3
 asdf_setup ruby 2.6.5
 asdf_setup rust 1.43.1
 asdf_setup tmux 3.1b
-asdf_setup yarn 1.19.1
-asdf_setup github-cli 0.6.4
+asdf_setup yarn 1.22.4
+asdf_setup github-cli 0.9.0
