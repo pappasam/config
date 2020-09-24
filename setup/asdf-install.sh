6b8df11b82233aa0e97084b2d51182871d489331
Sam Roeca <samuel.roeca@gmail.com>
2020-09-24 10:22:41 -0700

    Update asdf installs

--- setup/asdf-install.sh
+++ setup/asdf-install.sh
@@ -5,10 +5,14 @@
 ###########################################################################
 # Custom
 
-asdf plugin-add nodejs
-bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
-asdf install nodejs 14.4.0
-asdf global nodejs 14.4.0
+asdf_setup_nodejs() {
+  asdf plugin-add nodejs
+  bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+  asdf install nodejs "$1"
+  asdf global nodejs "$1"
+}
+
+asdf_setup_nodejs 14.12.0
 
 ###########################################################################
 # Generalized
@@ -19,20 +23,21 @@ asdf_setup() {
   asdf global "$1" "$2"
 }
 
+asdf_setup direnv 2.22.0
+asdf_setup github-cli 0.9.0
+asdf_setup gohugo extended_0.74.3
 asdf_setup golang 1.14.4
 asdf_setup java adopt-openjdk-13.0.2+8
 asdf_setup neovim nightly
 asdf_setup php 7.4.7
-asdf_setup python 3.8.3
+asdf_setup postgres 13.0
+asdf_setup python 3.8.6
 asdf_setup ruby 2.6.5
 asdf_setup rust 1.43.1
-asdf_setup tmux 3.1b
+asdf_setup tmux 3.2-rc
 asdf_setup yarn 1.22.4
-asdf_setup github-cli 0.9.0
-asdf_setup gohugo extended_0.74.3
-asdf_setup direnv 2.22.0
 
 # kubernetes
-asdf_setup minikube 1.11.0
 asdf_setup kubectl 1.19.0-beta.2
+asdf_setup minikube 1.11.0
 asdf_setup skaffold 1.11.0
