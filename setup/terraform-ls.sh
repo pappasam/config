7f1b791ec620259942e122cb9e6933d2cf6688eb
Sam Roeca <samuel.roeca@gmail.com>
2021-06-10 20:31:26 -0700

    terraform-ls is now supported by asdf

--- setup/terraform-ls.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION="0.18.0"
-DOWNLOAD_FILE="$HOME/Downloads/terraform.zip"
-
-curl -o "$DOWNLOAD_FILE" \
-  "https://releases.hashicorp.com/terraform-ls/$VERSION/terraform-ls_${VERSION}_linux_amd64.zip"
-
-unzip -d "$HOME/bin" "$DOWNLOAD_FILE"
