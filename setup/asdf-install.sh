6b217954fdce55fb1cccf9b5b7fa81a7ca4caba7
Sam Roeca <samuel.roeca@gmail.com>
2021-10-29 09:24:58 -0700

    Add useful programs to asdf installation suggestions

--- setup/asdf-install.sh
+++ setup/asdf-install.sh
@@ -32,7 +32,6 @@ asdf_setup() {
   asdf install "$plugin" "$version" && asdf global "$plugin" "$version"
 }
 
-asdf_setup nodejs latest
 asdf_setup direnv latest
 asdf_setup fzf latest
 asdf_setup github-cli latest
@@ -40,7 +39,9 @@ asdf_setup gohugo extended_0.74.3
 asdf_setup golang latest
 asdf_setup java adopt-openjdk-13.0.2+8
 asdf_setup neovim nightly
+asdf_setup nodejs latest
 asdf_setup php latest
+asdf_setup poetry latest
 asdf_setup postgres 13.0
 asdf_setup python latest
 asdf_setup ruby latest
@@ -48,6 +49,7 @@ asdf_setup rust latest
 asdf_setup shellcheck latest
 asdf_setup shfmt latest
 asdf_setup sqlite latest
+asdf_setup terraform latest
 asdf_setup terraform-ls latest
 asdf_setup tflint latest
 asdf_setup tfsec latest
