c0ec7b06c57277b7a238fb7052017be9001d029d
Sam Roeca <samuel.roeca@gmail.com>
2022-06-29 12:35:54 -0700

    Remove hugo from asdf, not used

--- scripts/ubuntu/asdf-install.sh
+++ scripts/ubuntu/asdf-install.sh
@@ -37,7 +37,6 @@ asdf_setup awscli latest
 asdf_setup direnv latest
 asdf_setup fzf latest
 asdf_setup github-cli latest
-asdf_setup gohugo extended_0.74.3
 asdf_setup golang latest
 asdf_setup java adopt-openjdk-13.0.2+8
 asdf_setup neovim nightly
