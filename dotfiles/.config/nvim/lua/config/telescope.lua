d46e979512024a6c39e756999917c06768f26bfc
Sam Roeca <samuel.roeca@gmail.com>
2022-07-13 11:19:43 -0700

    Telescope: ignore git, venv, and node_modules

--- dotfiles/.config/nvim/lua/config/telescope.lua
+++ dotfiles/.config/nvim/lua/config/telescope.lua
@@ -4,16 +4,21 @@ local actions = require('telescope.actions')
 
 ts.setup({
   defaults = {
-    prompt_prefix = " ",
-    layout_strategy = "vertical",
+    file_ignore_patterns = {
+      "node_modules",
+      ".git",
+      ".venv",
+    },
     layout_config = {
       vertical = { width = 0.90 },
     },
+    layout_strategy = "vertical",
     mappings = {
       i = {
         ['<esc>'] = actions.close,
         ['<C-u>'] = false,
       },
     },
+    prompt_prefix = " ",
   },
 })
