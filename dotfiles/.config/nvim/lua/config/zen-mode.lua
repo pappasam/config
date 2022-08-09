90e0d68234a32210dbb19e307bce3b36c14b08c9
Sam Roeca <samuel.roeca@gmail.com>
2022-08-09 08:24:36 -0700

    zen-mode: colorcolumn=0, wrap

--- dotfiles/.config/nvim/lua/config/zen-mode.lua
+++ dotfiles/.config/nvim/lua/config/zen-mode.lua
@@ -3,11 +3,13 @@ require("zen-mode").setup({
     width = 79,
     options = {
       signcolumn = "no",
+      wrap = true,
       number = false,
       relativenumber = false,
       cursorline = false,
       cursorcolumn = false,
       foldcolumn = "0",
+      colorcolumn = "0",
       list = false,
     },
   },
