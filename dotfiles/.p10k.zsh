cd5b2031440f3fbadca5d9d4b9b1184d9e64b09e
Sam Roeca <samuel.roeca@gmail.com>
2022-10-27 10:13:46 -0700

    Add Python version to p10k virtualenv prompt

--- dotfiles/.p10k.zsh
+++ dotfiles/.p10k.zsh
@@ -882,8 +882,8 @@
   ###[ virtualenv: python virtual environment (https://docs.python.org/3/library/venv.html) ]###
   # Python virtual environment color.
   typeset -g POWERLEVEL9K_VIRTUALENV_FOREGROUND=37
-  # Don't show Python version next to the virtual environment name.
-  typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=false
+  # Show Python version next to the virtual environment name.
+  typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=true
   # If set to "false", won't show virtualenv if pyenv is already shown.
   # If set to "if-different", won't show virtualenv if it's the same as pyenv.
   typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_WITH_PYENV=false
