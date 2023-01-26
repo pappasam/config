6b7645b41d59714d5ecf4aabbe1a2ea35950ea87
Sam Roeca <samuel.roeca@gmail.com>
2023-01-26 09:49:33 -0800

    Fix file_ignore_patterns for telescope

--- dotfiles/.config/nvim/lua/config/telescope.lua
+++ dotfiles/.config/nvim/lua/config/telescope.lua
@@ -5,9 +5,9 @@ local actions = require("telescope.actions")
 ts.setup({
   defaults = {
     file_ignore_patterns = {
-      "node_modules",
-      ".git",
-      ".venv",
+      "^node_modules/",
+      "^%.git/",
+      "^%.venv/",
     },
     layout_strategy = "flex",
     mappings = {
