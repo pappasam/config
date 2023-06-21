a1e68e232fa66b7e8f518927bb70188174f19a33
Sam Roeca <samuel.roeca@gmail.com>
2023-06-21 16:09:43 -0700

    No need for custom header logic anymore!

--- dotfiles/.config/nvim/after/queries/markdown/highlights.scm
+++
@@ -1,7 +0,0 @@
-; extends
-(atx_heading (atx_h1_marker) @text.title.h1 (inline) @text.title.h1)
-(atx_heading (atx_h2_marker) @text.title.h2 (inline) @text.title.h2)
-(atx_heading (atx_h3_marker) @text.title.h3 (inline) @text.title.h3)
-(atx_heading (atx_h4_marker) @text.title.h4 (inline) @text.title.h4)
-(atx_heading (atx_h5_marker) @text.title.h5 (inline) @text.title.h5)
-(atx_heading (atx_h6_marker) @text.title.h6 (inline) @text.title.h6)
