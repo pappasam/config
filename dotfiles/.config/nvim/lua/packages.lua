f3f5c2af110f6fc24d774f63ae4c1857cddd055a
Sam Roeca <samuel.roeca@gmail.com>
2023-11-03 19:19:36 -0700

    Support lua

--- dotfiles/.config/nvim/lua/packages.lua
+++ dotfiles/.config/nvim/lua/packages.lua
@@ -271,6 +271,37 @@ lspconfig.pyright.setup({
   capabilities = cmp_capabilities,
 })
 
+lspconfig.lua_ls.setup({
+  capabilities = cmp_capabilities,
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if
+      not vim.loop.fs_stat(path .. "/.luarc.json")
+      and not vim.loop.fs_stat(path .. "/.luarc.jsonc")
+    then
+      client.config.settings =
+        vim.tbl_deep_extend("force", client.config.settings, {
+          Lua = {
+            runtime = {
+              version = "LuaJIT",
+            },
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+              },
+            },
+          },
+        })
+      client.notify(
+        "workspace/didChangeConfiguration",
+        { settings = client.config.settings }
+      )
+    end
+    return true
+  end,
+})
+
 -- }}}
 -- symbols-outline.nvim_lsp {{{
 -- https://github.com/simrat39/symbols-outline.nvim_lsp
