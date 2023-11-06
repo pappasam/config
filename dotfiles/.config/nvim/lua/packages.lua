e27eecf9f95645667dd128b6722784c0f1abd6be
Sam Roeca <samuel.roeca@gmail.com>
2023-11-05 21:28:35 -0800

    Hover and signature help use same borders and cmp

--- dotfiles/.config/nvim/lua/packages.lua
+++ dotfiles/.config/nvim/lua/packages.lua
@@ -263,6 +263,16 @@ cmp.setup({
 local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- }}}
+-- lsp.txt {{{
+-- :help lsp.txt
+
+vim.lsp.handlers["textDocument/hover"] =
+  vim.lsp.with(vim.lsp.handlers.hover, cmp.config.window.bordered())
+
+vim.lsp.handlers["textDocument/signatureHelp"] =
+  vim.lsp.with(vim.lsp.handlers.signature_help, cmp.config.window.bordered())
+
+-- }}}
 -- nvim-lspconfig {{{
 -- https://github.com/neovim/nvim-lspconfig
 
