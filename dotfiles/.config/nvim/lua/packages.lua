583b2060085a6f21ff5214847929f9ff888a4587
Sam Roeca <samuel.roeca@gmail.com>
2023-11-16 10:26:08 -0800

    Fix cmp mappings for scrolling docs

--- dotfiles/.config/nvim/lua/packages.lua
+++ dotfiles/.config/nvim/lua/packages.lua
@@ -17,8 +17,8 @@ cmp.setup({
     documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-k>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-j>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
     ["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
