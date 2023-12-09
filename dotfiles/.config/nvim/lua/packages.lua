354d8f89fd14baed7518c9df6af68d4e03219c2d
Sam Roeca <samuel.roeca@gmail.com>
2023-12-09 13:57:11 -0800

    Make colorizer selection more specific

--- dotfiles/.config/nvim/lua/packages.lua
+++ dotfiles/.config/nvim/lua/packages.lua
@@ -281,9 +281,13 @@ require("ts_context_commentstring").setup()
 require("colorizer").setup({
   filetypes = { "*" },
   user_default_options = {
+    RGB = true, -- #RGB hex codes
+    RRGGBB = true, -- #RRGGBB hex codes
+    names = false, -- "Name" codes like Blue or blue
     RRGGBBAA = true, -- #RRGGBBAA hex codes
     AARRGGBB = true, -- 0xAARRGGBB hex codes
-    css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+    rgb_fn = true, -- CSS rgb() and rgba() functions
+    hsl_fn = true, -- CSS hsl() and hsla() functions
     sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
   },
 })
