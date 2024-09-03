34b8a5eb7da09fecca0481c2480ac7c57c6bf3ea
Sam Roeca <samuel.roeca@gmail.com>
2024-09-03 16:20:28 -0400

    Add link to inspiration

--- dotfiles/.config/nvim/lua/custom_snippets.lua
+++ dotfiles/.config/nvim/lua/custom_snippets.lua
@@ -1,3 +1,6 @@
+-- Inspiration:
+-- https://www.reddit.com/r/neovim/comments/1cxfhom/builtin_snippets_so_good_i_removed_luasnip/
+
 local global_snippets = {
   { trigger = "shebang", body = "#!/bin/sh" },
 }
