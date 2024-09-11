0d8fb0aec743ce9334799d684eda84514612d693
Sam Roeca <samuel.roeca@gmail.com>
2024-09-11 10:43:48 -0400

    Add firefox deb install instructions into notes

---
+++ scripts/ubuntu/firefox-deb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+#
+# Steps obtained from:
+# https://www.omgubuntu.co.uk/2022/04/how-to-install-firefox-deb-apt-ubuntu-22-04
+
+sudo snap remove firefox
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+
+Package: firefox*
+Pin: release o=Ubuntu
+Pin-Priority: -1' | sudo tee /etc/apt/preferences.d/mozilla
+sudo apt update && sudo apt install firefox
