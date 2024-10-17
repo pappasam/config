b2bd658de56cd6660c0f33333771c891e00b8327
Sam Roeca <samuel.roeca@gmail.com>
2024-10-17 15:22:28 -0400

    Add session token

--- dotfiles/.config/nvim/lua/packages.lua
+++ dotfiles/.config/nvim/lua/packages.lua
@@ -167,7 +167,7 @@ vim.api.nvim_create_autocmd("FileType", {
       root_dir = vim.fs.root(args.buf, { ".github", ".git" }),
       capabilities = default_capabilities,
       init_options = {
-        session_token = nil,
+        session_token = os.getenv("GITHUB_ACTIONS_LS_TOKEN"),
       },
     })
   end,
