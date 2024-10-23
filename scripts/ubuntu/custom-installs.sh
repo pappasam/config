56247a69bc58996291a827ad6751e9d43dcb79ac
Sam Roeca <samuel.roeca@gmail.com>
2024-10-23 13:08:00 -0400

    Simplify install script

--- scripts/ubuntu/custom-installs.sh
+++ scripts/ubuntu/custom-installs.sh
@@ -2,39 +2,30 @@
 
 set -euo pipefail
 
-function echo_bold_italic_underline() {
-  echo -e "\e[3m\e[1m\e[4m$1\e[0m"
-}
+if ! command -v zinit >/dev/null; then
+  echo 'Installing zinit'
+  git clone https://github.com/zdharma-continuum/zinit.git ~/.local/share/zinit/zinit.git
+fi
 
-function github_install() {
-  local git_remote_url="$1"
-  local local_path="$2"
-  if [ ! -d "$local_path" ]; then
-    echo_bold_italic_underline "Downloading $git_remote_url"
-    git clone "$git_remote_url" "$local_path"
-  fi
-}
-
-github_install https://github.com/zdharma-continuum/zinit.git ~/.local/share/zinit/zinit.git
-
-if ! command -v docker > /dev/null; then
+if ! command -v docker >/dev/null; then
   # See: https://github.com/docker/docker-install
-  echo_bold_italic_underline 'Installing docker, follow prompted instructions'
+  echo 'Installing docker, follow prompted instructions'
   curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
   sh /tmp/get-docker.sh
   sudo usermod -aG docker "$(whoami)"
 fi
 
-if ! command -v rustup > /dev/null; then
-  echo_bold_italic_underline 'Installing Rust'
+if ! command -v rustup >/dev/null; then
+  echo 'Installing Rust'
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
-if ! command -v ghcup > /dev/null; then
-  echo_bold_italic_underline 'Installing ghcup (haskell tooling)'
+if ! command -v ghcup >/dev/null; then
+  echo 'Installing ghcup (haskell tooling)'
   curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 fi
 
-# install mise
-curl https://mise.run | sh
-echo 'now run "mise install"'
+if ! command -v mise >/dev/null; then
+  echo 'Installing mise'
+  curl https://mise.run | sh
+fi
