84d2dab44e38ed11ea22901b393af7e1acc5733a
Sam Roeca <samuel.roeca@gmail.com>
2024-10-23 13:12:52 -0400

    Make custom-installs verbose and consistent

--- scripts/ubuntu/custom-installs.sh
+++ scripts/ubuntu/custom-installs.sh
@@ -2,30 +2,30 @@
 
 set -euo pipefail
 
-if ! command -v zinit >/dev/null; then
-  echo 'Installing zinit'
+if test ! -d ~/.local/share/zinit/zinit.git; then
+  echo 'INSTALLING: zinit'
   git clone https://github.com/zdharma-continuum/zinit.git ~/.local/share/zinit/zinit.git
 fi
 
 if ! command -v docker >/dev/null; then
   # See: https://github.com/docker/docker-install
-  echo 'Installing docker, follow prompted instructions'
+  echo 'INSTALLING: docker, follow prompted instructions'
   curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
   sh /tmp/get-docker.sh
   sudo usermod -aG docker "$(whoami)"
 fi
 
 if ! command -v rustup >/dev/null; then
-  echo 'Installing Rust'
+  echo 'INSTALLING: Rust'
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
 if ! command -v ghcup >/dev/null; then
-  echo 'Installing ghcup (haskell tooling)'
+  echo 'INSTALLING: ghcup (haskell tooling)'
   curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 fi
 
 if ! command -v mise >/dev/null; then
-  echo 'Installing mise'
+  echo 'INSTALLING: mise-en-place'
   curl https://mise.run | sh
 fi
