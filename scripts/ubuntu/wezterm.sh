07ff191bbfa679bf4f48428d65fede45a788883d
Sam Roeca <samuel.roeca@gmail.com>
2024-11-01 10:08:37 -0400

    Remove wezterm installation file

--- scripts/ubuntu/wezterm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# https://wezfurlong.org/wezterm/install/linux.html#__tabbed_1_3
-
-set -euo pipefail
-
-curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
-sudo apt update
-sudo apt install wezterm-nightly
