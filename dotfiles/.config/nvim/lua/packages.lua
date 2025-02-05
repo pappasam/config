5bac86f60d49c99d96a99603d5281226bca0a67a
Sam Roeca <samuel.roeca@gmail.com>
2025-02-05 15:05:39 -0500

    Revert "nvim-tree: enable floating window"
    This reverts commit b8d3d33236a10c06305c062ba271c5d65e73191b.

--- dotfiles/.config/nvim/lua/packages.lua
+++ dotfiles/.config/nvim/lua/packages.lua
@@ -481,29 +481,6 @@ require("nvim-tree").setup({ -- https://github.com/kyazdani42/nvim-tree.lua {{{
   },
   view = {
     signcolumn = "no",
-    float = {
-      enable = true,
-      open_win_config = function()
-        local screen_w = vim.opt.columns:get()
-        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-        local window_w = screen_w * 0.9
-        local window_h = screen_h * 0.9
-        local window_w_int = math.floor(window_w)
-        local window_h_int = math.floor(window_h)
-        local center_x = (screen_w - window_w) / 2
-        local center_y = (
-          ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
-        )
-        return {
-          border = "rounded",
-          relative = "editor",
-          row = center_y,
-          col = center_x,
-          width = window_w_int,
-          height = window_h_int,
-        }
-      end,
-    },
   },
   renderer = {
     full_name = true,
