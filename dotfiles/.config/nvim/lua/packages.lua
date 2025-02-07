9ffec620efb3e222902bdf975d6d068aa233c45b
Sam Roeca <samuel.roeca@gmail.com>
2025-02-07 15:03:08 -0500

    nvim: lazy-load lua modules that can be lazy loaded

--- dotfiles/.config/nvim/lua/packages.lua
+++ dotfiles/.config/nvim/lua/packages.lua
@@ -336,200 +336,202 @@ vim.treesitter.query.set(
 )
 
 -- }}}
-require("aerial").setup({ -- https://github.com/stevearc/aerial.nvim {{{
-}) -- }}}
-require("blink-cmp").setup({ -- https://github.com/Saghen/blink.cmp {{{
-  sources = {
-    cmdline = {}, -- disables cmdline completions
-  },
-}) -- }}}
-require("colorizer").setup({ -- https://github.com/catgoose/nvim-colorizer.lua {{{
-}) -- }}}
-require("faster").setup({ -- https://github.com/pteroctopus/faster.nvim {{{
-}) -- }}}
-require("fidget").setup({ -- https://github.com/j-hui/fidget.nvim {{{
-  progress = {
-    suppress_on_insert = true,
-  },
-}) -- }}}
-require("gitsigns").setup({ -- https://github.com/lewis6991/gitsigns.nvim {{{
-  signcolumn = false,
-  numhl = true,
-  on_attach = function(bufnr)
-    local gs = package.loaded.gitsigns
+vim.defer_fn(function() -- lazy loading
+  require("aerial").setup({ -- https://github.com/stevearc/aerial.nvim {{{
+  }) -- }}}
+  require("blink-cmp").setup({ -- https://github.com/Saghen/blink.cmp {{{
+    sources = {
+      cmdline = {}, -- disables cmdline completions
+    },
+  }) -- }}}
+  require("colorizer").setup({ -- https://github.com/catgoose/nvim-colorizer.lua {{{
+  }) -- }}}
+  require("faster").setup({ -- https://github.com/pteroctopus/faster.nvim {{{
+  }) -- }}}
+  require("fidget").setup({ -- https://github.com/j-hui/fidget.nvim {{{
+    progress = {
+      suppress_on_insert = true,
+    },
+  }) -- }}}
+  require("gitsigns").setup({ -- https://github.com/lewis6991/gitsigns.nvim {{{
+    signcolumn = false,
+    numhl = true,
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
 
-    local function map(mode, l, r, opts)
-      opts = opts or {}
-      opts.buffer = bufnr
-      vim.keymap.set(mode, l, r, opts)
-    end
-
-    -- Navigation
-    map("n", "]g", function()
-      if vim.wo.diff then
-        return "]g"
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
       end
-      vim.schedule(function()
-        gs.next_hunk()
+
+      -- Navigation
+      map("n", "]g", function()
+        if vim.wo.diff then
+          return "]g"
+        end
+        vim.schedule(function()
+          gs.next_hunk()
+        end)
+        return "<Ignore>"
+      end, { expr = true })
+
+      map("n", "[g", function()
+        if vim.wo.diff then
+          return "[g"
+        end
+        vim.schedule(function()
+          gs.prev_hunk()
+        end)
+        return "<Ignore>"
+      end, { expr = true })
+
+      -- Actions
+      map({ "n", "v" }, "<leader>hs", "<Cmd>Gitsigns stage_hunk<CR>")
+      map({ "n", "v" }, "<leader>hr", "<Cmd>Gitsigns reset_hunk<CR>")
+      map("n", "<leader>hS", gs.stage_buffer)
+      map("n", "<leader>hu", gs.undo_stage_hunk)
+      map("n", "<leader>hR", gs.reset_buffer)
+      map("n", "<leader>hp", gs.preview_hunk)
+      map("n", "<leader>hb", function()
+        gs.blame_line({ full = true })
       end)
-      return "<Ignore>"
-    end, { expr = true })
-
-    map("n", "[g", function()
-      if vim.wo.diff then
-        return "[g"
-      end
-      vim.schedule(function()
-        gs.prev_hunk()
+      map("n", "<leader>tb", gs.toggle_current_line_blame)
+      map("n", "<leader>hd", gs.diffthis)
+      map("n", "<leader>hD", function()
+        gs.diffthis("~")
       end)
-      return "<Ignore>"
-    end, { expr = true })
+      map("n", "<leader>td", gs.toggle_deleted)
 
-    -- Actions
-    map({ "n", "v" }, "<leader>hs", "<Cmd>Gitsigns stage_hunk<CR>")
-    map({ "n", "v" }, "<leader>hr", "<Cmd>Gitsigns reset_hunk<CR>")
-    map("n", "<leader>hS", gs.stage_buffer)
-    map("n", "<leader>hu", gs.undo_stage_hunk)
-    map("n", "<leader>hR", gs.reset_buffer)
-    map("n", "<leader>hp", gs.preview_hunk)
-    map("n", "<leader>hb", function()
-      gs.blame_line({ full = true })
-    end)
-    map("n", "<leader>tb", gs.toggle_current_line_blame)
-    map("n", "<leader>hd", gs.diffthis)
-    map("n", "<leader>hD", function()
-      gs.diffthis("~")
-    end)
-    map("n", "<leader>td", gs.toggle_deleted)
-
-    -- Text object
-    map({ "o", "x" }, "ih", "<Cmd>Gitsigns select_hunk<CR>")
-  end,
-}) -- }}}
-require("gx").setup({ -- https://github.com/chrishrb/gx.nvim {{{
-  open_browser_app = "firefox",
-  handlers = {
-    cratesio = {
-      name = "cratesio",
-      filename = "Cargo.toml",
-      handle = function(mode, line, _)
-        local pkg = require("gx.helper").find(line, mode, "([^=%s]+)%s-=%s")
-        if pkg then
-          return "https://crates.io/crates/" .. pkg
-        end
-      end,
-    },
-    pypi = {
-      name = "pypi",
-      filename = "pyproject.toml",
-      handle = function(mode, line, _)
-        local pkg = require("gx.helper").find(line, mode, "([^=%s]+)%s-=%s")
-        if pkg then
-          return "https://pypi.org/project/" .. pkg
-        end
-      end,
-    },
-    ruff = {
-      name = "ruff",
-      filetypes = { "python" },
-      handle = function(mode, line, _)
-        local rule =
-          require("gx.helper").find(line, mode, "# noqa: ([A-Z][0-9]+)")
-        if rule then
-          return "https://docs.astral.sh/ruff/rules/" .. rule
-        end
-      end,
-    },
-    npmjs = {
-      name = "npmjs",
-      filename = "package.json",
-      handle = function(mode, line, _)
-        local pkg = require("gx.helper").find(line, mode, '"([^"]+)":')
-        if pkg then
-          return "https://www.npmjs.com/package/" .. pkg
-        end
-      end,
-    },
-  },
-}) -- }}}
-require("ibl").setup({ -- https://github.com/lukas-reineke/indent-blankline.nvim {{{
-  indent = {
-    char = "▏",
-  },
-  scope = {
-    enabled = false,
-  },
-}) -- }}}
-require("nvim-autopairs").setup({ -- https://github.com/windwp/nvim-autopairs {{{
-  map_c_h = true,
-  map_c_w = true,
-  map_cr = true,
-  enable_check_bracket_line = false,
-}) -- }}}
-require("nvim-tree").setup({ -- https://github.com/kyazdani42/nvim-tree.lua {{{
-  disable_netrw = true,
-  filters = {
-    dotfiles = true,
-    custom = {
-      "/__pycache__",
-    },
-    exclude = {
-      "/.github",
-      "/dotfiles",
-      "/instance",
-    },
-  },
-  view = {
-    signcolumn = "no",
-  },
-  renderer = {
-    full_name = true,
-    symlink_destination = false,
-    root_folder_label = false,
-  },
-}) -- }}}
-require("ts-autotag").setup({ -- https://github.com/tronikelis/ts-autotag.nvim {{{
-}) -- }}}
-require("nvim-web-devicons").setup({ -- https://github.com/kyazdani42/nvim-web-devicons {{{
-  -- globally enable default icons (default to false)
-  -- will get overriden by `get_icons` option
-  default = true,
-}) -- }}}
-require("presenting").setup({ -- https://github.com/sotte/presenting.nvim {{{
-  options = {
-    width = 60,
-  },
-  separator = {
-    markdown = "^##? ", -- # or ##, but not ###+
-  },
-  configure_slide_buffer = function(_)
-    vim.cmd([[
-      Fidget suppress
-      setlocal buftype=nofile filetype=markdown bufhidden=wipe nomodifiable wrap conceallevel=3 concealcursor=nc
-      nnoremap <buffer> q <Cmd>Presenting<CR>
-      nnoremap <buffer> <C-w> <NOP>
-      cnoreabbrev <buffer> q Presenting
-      echo
-    ]])
-  end,
-}) -- }}}
-require("telescope").setup({ -- https://github.com/nvim-telescope/telescope.nvim {{{
-  defaults = {
-    file_ignore_patterns = {
-      "^node_modules/",
-      "^%.git/",
-      "^%.venv/",
-    },
-    layout_strategy = "flex",
-    layout_config = {
-      height = 0.9,
-      width = 0.9,
-    },
-    mappings = {
-      i = {
-        ["<esc>"] = require("telescope.actions").close,
+      -- Text object
+      map({ "o", "x" }, "ih", "<Cmd>Gitsigns select_hunk<CR>")
+    end,
+  }) -- }}}
+  require("gx").setup({ -- https://github.com/chrishrb/gx.nvim {{{
+    open_browser_app = "firefox",
+    handlers = {
+      cratesio = {
+        name = "cratesio",
+        filename = "Cargo.toml",
+        handle = function(mode, line, _)
+          local pkg = require("gx.helper").find(line, mode, "([^=%s]+)%s-=%s")
+          if pkg then
+            return "https://crates.io/crates/" .. pkg
+          end
+        end,
+      },
+      pypi = {
+        name = "pypi",
+        filename = "pyproject.toml",
+        handle = function(mode, line, _)
+          local pkg = require("gx.helper").find(line, mode, "([^=%s]+)%s-=%s")
+          if pkg then
+            return "https://pypi.org/project/" .. pkg
+          end
+        end,
+      },
+      ruff = {
+        name = "ruff",
+        filetypes = { "python" },
+        handle = function(mode, line, _)
+          local rule =
+            require("gx.helper").find(line, mode, "# noqa: ([A-Z][0-9]+)")
+          if rule then
+            return "https://docs.astral.sh/ruff/rules/" .. rule
+          end
+        end,
+      },
+      npmjs = {
+        name = "npmjs",
+        filename = "package.json",
+        handle = function(mode, line, _)
+          local pkg = require("gx.helper").find(line, mode, '"([^"]+)":')
+          if pkg then
+            return "https://www.npmjs.com/package/" .. pkg
+          end
+        end,
       },
     },
-    prompt_prefix = " ",
-  },
-}) -- }}}
+  }) -- }}}
+  require("ibl").setup({ -- https://github.com/lukas-reineke/indent-blankline.nvim {{{
+    indent = {
+      char = "▏",
+    },
+    scope = {
+      enabled = false,
+    },
+  }) -- }}}
+  require("nvim-autopairs").setup({ -- https://github.com/windwp/nvim-autopairs {{{
+    map_c_h = true,
+    map_c_w = true,
+    map_cr = true,
+    enable_check_bracket_line = false,
+  }) -- }}}
+  require("nvim-tree").setup({ -- https://github.com/kyazdani42/nvim-tree.lua {{{
+    disable_netrw = true,
+    filters = {
+      dotfiles = true,
+      custom = {
+        "/__pycache__",
+      },
+      exclude = {
+        "/.github",
+        "/dotfiles",
+        "/instance",
+      },
+    },
+    view = {
+      signcolumn = "no",
+    },
+    renderer = {
+      full_name = true,
+      symlink_destination = false,
+      root_folder_label = false,
+    },
+  }) -- }}}
+  require("ts-autotag").setup({ -- https://github.com/tronikelis/ts-autotag.nvim {{{
+  }) -- }}}
+  require("nvim-web-devicons").setup({ -- https://github.com/kyazdani42/nvim-web-devicons {{{
+    -- globally enable default icons (default to false)
+    -- will get overriden by `get_icons` option
+    default = true,
+  }) -- }}}
+  require("presenting").setup({ -- https://github.com/sotte/presenting.nvim {{{
+    options = {
+      width = 60,
+    },
+    separator = {
+      markdown = "^##? ", -- # or ##, but not ###+
+    },
+    configure_slide_buffer = function(_)
+      vim.cmd([[
+        Fidget suppress
+        setlocal buftype=nofile filetype=markdown bufhidden=wipe nomodifiable wrap conceallevel=3 concealcursor=nc
+        nnoremap <buffer> q <Cmd>Presenting<CR>
+        nnoremap <buffer> <C-w> <NOP>
+        cnoreabbrev <buffer> q Presenting
+        echo
+      ]])
+    end,
+  }) -- }}}
+  require("telescope").setup({ -- https://github.com/nvim-telescope/telescope.nvim {{{
+    defaults = {
+      file_ignore_patterns = {
+        "^node_modules/",
+        "^%.git/",
+        "^%.venv/",
+      },
+      layout_strategy = "flex",
+      layout_config = {
+        height = 0.9,
+        width = 0.9,
+      },
+      mappings = {
+        i = {
+          ["<esc>"] = require("telescope.actions").close,
+        },
+      },
+      prompt_prefix = " ",
+    },
+  }) -- }}}
+end, 100)
