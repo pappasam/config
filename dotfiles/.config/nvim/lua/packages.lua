290b9860f3e9aa0fe3c2860ced49c909c10f4c0b
Sam Roeca <samuel.roeca@gmail.com>
2025-02-10 15:49:02 -0500

    Remove img-clip, kind of annoying

--- dotfiles/.config/nvim/lua/packages.lua
+++ dotfiles/.config/nvim/lua/packages.lua
@@ -549,13 +549,4 @@ vim.defer_fn(function() -- lazy loading
       prompt_prefix = " ",
     },
   }) -- }}}
-  require("img-clip").setup({ -- https://github.com/HakonHarnes/img-clip.nvim {{{
-    default = {
-      embed_image_as_base64 = false,
-      prompt_for_file_name = false,
-      drag_and_drop = {
-        insert_mode = true,
-      },
-    },
-  }) -- }}}
 end, 100)
