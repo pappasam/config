4edc734cd4b2fefe1adeabccf0c0d1a4b371d63b
Sam Roeca <samuel.roeca@gmail.com>
2025-03-17 11:48:26 -0400

    Snacks dashboard

--- dotfiles/.config/nvim/lua/packages.lua
+++ dotfiles/.config/nvim/lua/packages.lua
@@ -307,6 +307,75 @@ require("snacks").setup({ -- https://github.com/folke/snacks.nvim {{{
   explorer = { enabled = true },
   gitbrowse = { enabled = true },
   image = { enabled = true },
+  dashboard = {
+    enabled = true,
+    sections = {
+      { section = "header" },
+      { section = "keys", gap = 1, padding = 1 },
+      {
+        pane = 2,
+        icon = " ",
+        desc = "Browse Repo",
+        padding = 1,
+        key = "b",
+        action = function()
+          Snacks.gitbrowse()
+        end,
+      },
+      function()
+        local in_git = Snacks.git.get_root() ~= nil
+        local cmds = {
+          {
+            title = "Notifications",
+            cmd = "gh notify -s -a -n5",
+            action = function()
+              vim.ui.open("https://github.com/notifications")
+            end,
+            key = "N",
+            icon = " ",
+            height = 5,
+            enabled = true,
+          },
+          {
+            title = "Open Issues",
+            cmd = "gh issue list -L 3",
+            key = "i",
+            action = function()
+              vim.fn.jobstart("gh issue list --web", { detach = true })
+            end,
+            icon = " ",
+            height = 7,
+          },
+          {
+            icon = " ",
+            title = "Open PRs",
+            cmd = "gh pr list -L 3",
+            key = "P",
+            action = function()
+              vim.fn.jobstart("gh pr list --web", { detach = true })
+            end,
+            height = 7,
+          },
+          {
+            icon = " ",
+            title = "Git Status",
+            cmd = "git --no-pager diff --stat -B -M -C",
+            height = 10,
+          },
+        }
+        return vim.tbl_map(function(cmd)
+          return vim.tbl_extend("force", {
+            pane = 2,
+            section = "terminal",
+            enabled = in_git,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          }, cmd)
+        end, cmds)
+      end,
+    },
+  },
   indent = {
     enabled = true,
     scope = {
