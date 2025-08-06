e64832d5b974d4246e48f9690dc264d229522d1a
Sam Roeca <samuel.roeca@gmail.com>
2025-08-06 09:58:56 -0400

    mini.files: toggle dotfiles

--- dotfiles/.config/nvim/lua/packages.lua
+++ dotfiles/.config/nvim/lua/packages.lua
@@ -452,6 +452,27 @@ _G.minifiles_toggle = function()
   end
 end
 
+local show_dotfiles = true
+local filter_show = function(fs_entry)
+  return true
+end
+local filter_hide = function(fs_entry)
+  return not vim.startswith(fs_entry.name, ".")
+end
+local toggle_dotfiles = function()
+  show_dotfiles = not show_dotfiles
+  local new_filter = show_dotfiles and filter_show or filter_hide
+  MiniFiles.refresh({ content = { filter = new_filter } })
+end
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesBufferCreate",
+  callback = function(args)
+    local buf_id = args.data.buf_id
+    -- Tweak left-hand side of mapping to your liking
+    vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+  end,
+})
+
 -- }}}
 -- tronikelis/ts-autotag.nvim {{{
 require("ts-autotag").setup({})
