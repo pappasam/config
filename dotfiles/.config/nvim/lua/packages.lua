f7c0fa888f28f606412b15c027109c2c13bb5af5
Sam Roeca <samuel.roeca@gmail.com>
2025-08-08 20:39:13 -0400

    nvim: add ability to open file in new tab

--- dotfiles/.config/nvim/lua/packages.lua
+++ dotfiles/.config/nvim/lua/packages.lua
@@ -316,23 +316,42 @@ _G.minifiles_toggle = function()
 end
 
 local show_dotfiles = false
+
 local filter_show = function(_)
   return true
 end
+
 local filter_hide = function(fs_entry)
   return not vim.startswith(fs_entry.name, ".")
 end
+
 local toggle_dotfiles = function()
   show_dotfiles = not show_dotfiles
   local new_filter = show_dotfiles and filter_show or filter_hide
   MiniFiles.refresh({ content = { filter = new_filter } })
 end
+
+local map_split = function(buf_id, lhs, direction)
+  local rhs = function()
+    local cur_target = MiniFiles.get_explorer_state().target_window
+    local new_target = vim.api.nvim_win_call(cur_target, function()
+      vim.cmd(direction .. " split")
+      return vim.api.nvim_get_current_win()
+    end)
+    MiniFiles.set_target_window(new_target)
+  end
+  local desc = "Split " .. direction
+  vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+end
+
 vim.api.nvim_create_autocmd("User", {
   pattern = "MiniFilesBufferCreate",
   callback = function(args)
     local buf_id = args.data.buf_id
-    -- Tweak left-hand side of mapping to your liking
     vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+    map_split(buf_id, "<C-s>", "belowright horizontal")
+    map_split(buf_id, "<C-v>", "belowright vertical")
+    map_split(buf_id, "<C-t>", "tab")
   end,
 })
 
