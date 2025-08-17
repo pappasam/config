b549795bb16c5ed9d80fc313f00febb9524131ac
Sam Roeca <samuel.roeca@gmail.com>
2025-08-16 15:35:40 -1000

    nvim: remove unncessary explicit config from telescope

--- dotfiles/.config/nvim/lua/packages.lua
+++ dotfiles/.config/nvim/lua/packages.lua
@@ -321,11 +321,6 @@ require("telescope").setup({
       "^%.git/",
       "^%.venv/",
     },
-    layout_strategy = "flex",
-    layout_config = {
-      height = 0.9,
-      width = 0.9,
-    },
     mappings = {
       i = {
         ["<esc>"] = require("telescope.actions").close,
