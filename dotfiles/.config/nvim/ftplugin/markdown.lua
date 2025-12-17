ca2f6bc09f9dcc9185d7206e5525bb02e144e089
Sam Roeca <samuel.roeca@gmail.com>
2025-12-17 14:33:37 -0500

    disable diagnostics on markdown files by default

--- dotfiles/.config/nvim/ftplugin/markdown.lua
+++ dotfiles/.config/nvim/ftplugin/markdown.lua
@@ -1,4 +1,5 @@
 vim.bo.keywordprg = ":DefEng"
 vim.bo.commentstring = "<!-- %s -->"
 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+vim.diagnostic.enable(false)
 vim.treesitter.start()
