839c59eb964f69e3d18aadd0396763f8c83f4051
Sam Roeca <samuel.roeca@gmail.com>
2026-01-22 06:49:21 -0500

    change markdown indentation to 4 spaces

--- dotfiles/.config/nvim/ftplugin/markdown.lua
+++ dotfiles/.config/nvim/ftplugin/markdown.lua
@@ -1,5 +1,9 @@
 vim.bo.keywordprg = ":DefEng"
 vim.bo.commentstring = "<!-- %s -->"
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+vim.bo.expandtab = true
 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 vim.diagnostic.enable(false)
 vim.treesitter.start()
