8dd63168f979a943d659b49118b3e45187fa44c7
Sam Roeca <samuel.roeca@gmail.com>
2026-09-06 11:54:03 -0400

    gnome: remove unnecessary newlines, easier to read without breaks

--- scripts/ubuntu/gnome-settings.sh
+++ scripts/ubuntu/gnome-settings.sh
@@ -32,8 +32,7 @@ gsettings set "$wm" close "['<Alt>F4', '<Primary><Alt>d']"
 gsettings set "$wm" minimize "['<Super>j']"
 
 # Remove conflicts with Super+M and Ctrl+Alt+D.
-gsettings set "$wm" show-desktop \
-  "['<Primary><Super>d', '<Super>d']"
+gsettings set "$wm" show-desktop "['<Primary><Super>d', '<Super>d']"
 gsettings set "$shell" toggle-message-tray "['<Super>v']"
 
 # Use GNOME's native half-screen tiling without an extension.
@@ -41,38 +40,27 @@ gsettings set "$shell" toggle-message-tray "['<Super>v']"
 # Disable Tiling Assistant so it cannot override the native bindings.
 gnome-extensions disable "$tiling_uuid"
 gsettings set org.gnome.mutter edge-tiling true
-gsettings set "$mutter" toggle-tiled-left \
-  "['<Super>Left', '<Super>KP_4', '<Super>h']"
-gsettings set "$mutter" toggle-tiled-right \
-  "['<Super>Right', '<Super>KP_6', '<Super>l']"
+gsettings set "$mutter" toggle-tiled-left "['<Super>Left', '<Super>KP_4', '<Super>h']"
+gsettings set "$mutter" toggle-tiled-right "['<Super>Right', '<Super>KP_6', '<Super>l']"
 
 # Restore the defaults (unbound) for the old non-resizing edge-push actions.
 gsettings reset "$wm" move-to-side-w
 gsettings reset "$wm" move-to-side-e
 
 # Move windows between workspaces.
-gsettings set "$wm" move-to-workspace-left \
-  "['<Super><Shift>Page_Up', '<Control><Shift><Alt>Left', '<Control><Shift><Alt>h']"
-gsettings set "$wm" move-to-workspace-right \
-  "['<Super><Shift>Page_Down', '<Control><Shift><Alt>Right', '<Control><Shift><Alt>l']"
+gsettings set "$wm" move-to-workspace-left "['<Super><Shift>Page_Up', '<Control><Shift><Alt>Left', '<Control><Shift><Alt>h']"
+gsettings set "$wm" move-to-workspace-right "['<Super><Shift>Page_Down', '<Control><Shift><Alt>Right', '<Control><Shift><Alt>l']"
 
 # Move windows between monitors.
-gsettings set "$wm" move-to-monitor-left \
-  "['<Super><Shift>Left', '<Super><Shift>h']"
-gsettings set "$wm" move-to-monitor-right \
-  "['<Super><Shift>Right', '<Super><Shift>l']"
-gsettings set "$wm" move-to-monitor-up \
-  "['<Super><Shift>Up', '<Super><Shift>k']"
-gsettings set "$wm" move-to-monitor-down \
-  "['<Super><Shift>Down', '<Super><Shift>j']"
+gsettings set "$wm" move-to-monitor-left "['<Super><Shift>Left', '<Super><Shift>h']"
+gsettings set "$wm" move-to-monitor-right "['<Super><Shift>Right', '<Super><Shift>l']"
+gsettings set "$wm" move-to-monitor-up "['<Super><Shift>Up', '<Super><Shift>k']"
+gsettings set "$wm" move-to-monitor-down "['<Super><Shift>Down', '<Super><Shift>j']"
 
 # Navigate workspaces.
-gsettings set "$wm" switch-to-workspace-left \
-  "['<Super>Page_Up', '<Control><Alt>Left', '<Control><Alt>h']"
-gsettings set "$wm" switch-to-workspace-right \
-  "['<Super>Page_Down', '<Control><Alt>Right', '<Control><Alt>l']"
-gsettings set "$wm" switch-to-workspace-1 \
-  "['<Super>Home', '<Control><Alt>1']"
+gsettings set "$wm" switch-to-workspace-left "['<Super>Page_Up', '<Control><Alt>Left', '<Control><Alt>h']"
+gsettings set "$wm" switch-to-workspace-right "['<Super>Page_Down', '<Control><Alt>Right', '<Control><Alt>l']"
+gsettings set "$wm" switch-to-workspace-1 "['<Super>Home', '<Control><Alt>1']"
 gsettings set "$wm" switch-to-workspace-2 "['<Control><Alt>2']"
 gsettings set "$wm" switch-to-workspace-3 "['<Control><Alt>3']"
 gsettings set "$wm" switch-to-workspace-4 "['<Control><Alt>4']"
@@ -90,8 +78,7 @@ gsettings set "$media" www "['<Control><Alt>b']"
 gsettings reset "$mutter" switch-monitor
 
 # Keyboard layout.
-gsettings set org.gnome.desktop.input-sources xkb-options \
-  "['ctrl:nocaps', 'compose:rwin']"
+gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps', 'compose:rwin']"
 
 # Prefer light application chrome. color-scheme covers modern GNOME/libadwaita
 # apps, while gtk-theme keeps older GTK apps from using the dark theme.
@@ -117,27 +104,14 @@ set_custom_shortcut() {
   gsettings set "$custom_schema:$path" binding "$binding"
 }
 
-set_custom_shortcut \
-  custom0 \
-  Kitty \
-  /home/sroeca/.local/bin/kitty \
-  '<Control><Alt>i'
+set_custom_shortcut custom0 Kitty /home/sroeca/.local/bin/kitty '<Control><Alt>i'
 
-set_custom_shortcut \
-  custom1 \
-  'Murmure Record Toggle' \
-  'murmure --transcription' \
-  Scroll_Lock
+set_custom_shortcut custom1 'Murmure Record Toggle' 'murmure --transcription' Scroll_Lock
 
 # Reserve the media-player key for Murmure, including the static system binding.
 gsettings set "$media" media '@as []'
 gsettings set "$media" media-static '@as []'
 
-set_custom_shortcut \
-  custom2 \
-  'Murmure Record Toggle2' \
-  'murmure --transcription' \
-  XF86AudioMedia
+set_custom_shortcut custom2 'Murmure Record Toggle2' 'murmure --transcription' XF86AudioMedia
 
-gsettings set "$media" custom-keybindings \
-  "['$custom_base/custom0/', '$custom_base/custom1/', '$custom_base/custom2/']"
+gsettings set "$media" custom-keybindings "['$custom_base/custom0/', '$custom_base/custom1/', '$custom_base/custom2/']"
